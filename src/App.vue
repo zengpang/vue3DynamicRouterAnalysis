f64a9e0d3bf60832157cb22eeb78426ea2f7a8b0
王千峻 <2516629927@qq.com>
2025-12-26 17:02:18 +0800

    更新

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Vue3动态路由分析测试程序</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
